e6f345730354c6748892874f3c101ec3de171168
fixorium <fixercoin.token@gmail.com>
2025-07-20 20:25:17 +0500

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,9 @@
+window.config = {
+  network: 'Fixorium',
+  rpc: 'https://rpc.fixorium.org',
+  rest: 'https://api.fixorium.org',
+  chainId: 'fixorium-devnet',
+  denom: 'fixorium',
+  displayDenom: 'FIXORIUM',
+  decimals: 6,
+};
